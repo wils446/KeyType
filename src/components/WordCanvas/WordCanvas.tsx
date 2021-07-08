9e9402f9b5cebab5a9d74a7a9212ac71cde96c39
wils446 <wilsonlimmmm@gmail.com>
2021-07-08 23:05:07 +0700

    add  component wordcanvas

---
+++ src/components/WordCanvas/WordCanvas.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+type WordCanvasProps = {
+    words: string;
+    html: string;
+};
+
+export default function WordCanvas({ words, html }: WordCanvasProps): JSX.Element {
+    return (
+        <div className="word-canvas my-3 py-1 px-3">
+            {html === "" ? <h2 className="words">{words}</h2> : <h2 dangerouslySetInnerHTML={{ __html: html }}></h2>}
+        </div>
+    );
+}
