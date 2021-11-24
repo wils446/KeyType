df0e74c9818d4ec4b1d141ed56bbec2663478480
wils446 <wilsonlimmmm@gmail.com>
2021-11-24 09:42:23 +0700

    create new hooks useDisplayWords

---
+++ src/hooks/useDisplayWords.ts
@@ -0,0 +1,34 @@
+import React from "react";
+
+const realtimeCheck = (words: string, inputValue: string) => {
+    let currentHtml = "";
+    for (let i = 0; i < words.length; i++) {
+        if (inputValue[i] === undefined)
+            currentHtml += `<span class="${inputValue.length === i ? "underline font-black" : "text-gray-500"}">${
+                words[i]
+            }</span>`;
+        else {
+            currentHtml += `<span class="text-black">${words[i]}</span>`;
+        }
+    }
+    return currentHtml;
+};
+
+export default function useDisplayWords(
+    words: string,
+    inputValue: string
+): [string, React.Dispatch<React.SetStateAction<string>>] {
+    const [input, setInput] = React.useState(inputValue);
+    const [html, setHtml] = React.useState("");
+
+    React.useEffect(
+        () => {
+            console.log(1);
+            setHtml(realtimeCheck(words, input));
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [input]
+    );
+
+    return [html, setInput];
+}
