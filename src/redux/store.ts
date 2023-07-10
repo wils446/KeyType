9b9848386ce6753da116275f52e275609aa5172e
Wilson <wilsonlimmmm@gmail.com>
2023-07-10 15:45:12 +0700

    add features/reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { scoreReducer, gameConfigReducer } from "./features";
 
-export const store = configureStore({ reducer: {} });
+export const store = configureStore({
+	reducer: {
+		scoreReducer,
+		gameConfigReducer,
+	},
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
