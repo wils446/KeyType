5ac1ebf034380fd4a76cd70c1a4e3c911fc178e3
Wilson <wilsonlimmmm@gmail.com>
2023-07-12 13:58:21 +0700

    add document meta

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import { selectGameConfigState } from "./redux/features";
 import { useAppSelector } from "./redux/hooks";
 import { Game, Score } from "./views";
+import DocumentMeta, { DocumentMetaProps } from "react-document-meta";
 
 function App() {
 	const { showScore } = useAppSelector(selectGameConfigState);
 
+	const meta: DocumentMetaProps = {
+		title: "KeyType",
+		description: "Simple Typing Game",
+	};
+
 	return (
-		<div className="h-screen w-screen bg-gradient-to-tr from-[#03002e] to-blue-950 flex flex-col items-center brand space-y-16">
-			<div className="basis-0"></div>
-			<h1 className="text-white text-7xl font-bold">KeyType</h1>
-			<div className="w-full flex justify-center">
-				{showScore ? <Score /> : <Game />}
+		<DocumentMeta {...meta}>
+			<div className="h-screen w-screen bg-gradient-to-tr from-[#03002e] to-blue-950 flex flex-col items-center brand space-y-16">
+				<div className="basis-0"></div>
+				<h1 className="text-white text-7xl font-bold">KeyType</h1>
+				<div className="w-full flex justify-center">
+					{showScore ? <Score /> : <Game />}
+				</div>
 			</div>
-		</div>
+		</DocumentMeta>
 	);
 }
 
